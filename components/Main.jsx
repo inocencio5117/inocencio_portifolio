81defc49f5fe20b3983d6efe0dd47f8d38611966
Vinicius Inocencio <vicnicius2009@gmail.com>
2022-06-13 21:46:12 -0300

    Feat: adding main component

---
+++ components/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <div className="w-full h-screen text-center">
+      <div className="max-w-[1240px] w=full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <p className="uppercase text-sm tracking-widest text-gray-600">
+            LET`S BUILD SOMETHING TOGHETER
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
