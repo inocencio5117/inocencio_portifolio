af921a05fecd25e39eb57ddd3337cb9981bf8ca0
Vinicius Inocencio <vicnicius2009@gmail.com>
2022-06-15 14:45:46 -0300

    Feat: adding navigation routes

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 function Contact() {
   return (
-    <div className="w-full lg:h-screen">
+    <div id="contact" className="w-full lg:h-screen">
       <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
         <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           Contact
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -34,30 +34,33 @@ const Navbar = () => {
       }
     >
       <div className="flex justify-between items-center w-full h-full px-2 2-xl:px-16">
-        <Image
-          src="/../public/assets/navLogo.png"
-          alt="/"
-          width="125"
-          height="50"
-        />
+        <Link href="/">
+          <Image
+            className="cursor-pointer"
+            src="/../public/assets/navLogo.png"
+            alt="/"
+            width="125"
+            height="50"
+          />
+        </Link>
 
         <div>
           <ul className="hidden md:flex">
             <Link href="/">
               <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
             </Link>
-            <Link href="/">
+            <Link href="/#abount">
               <li className="ml-10 text-sm uppercase hover:border-b">About</li>
             </Link>
-            <Link href="/">
+            <Link href="/#skills">
               <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
             </Link>
-            <Link href="/">
+            <Link href="/#projects">
               <li className="ml-10 text-sm uppercase hover:border-b">
                 Projects
               </li>
             </Link>
-            <Link href="/">
+            <Link href="/#contact">
               <li className="ml-10 text-sm uppercase hover:border-b">
                 Contact
               </li>
