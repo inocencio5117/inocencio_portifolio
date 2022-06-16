747696d0ed0898dadf0067343280fabd6a4347b4
Vinicius Inocencio <vicnicius2009@gmail.com>
2022-06-15 21:42:20 -0300

    Feat: refactoring Skills component

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Image from "next/image";
 
+import SkillItem from "./SkillItem";
+
 function Skills() {
   return (
     <div id="skills" className="w-full lg:h-screen p-2">
@@ -12,141 +14,45 @@ function Skills() {
         <h2 className="py-4">What I Can Do</h2>
 
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8 ">
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="/../public/assets/skills/html.png"
-                  width="64px"
-                  height="64px"
-                  alt="/"
-                />
-              </div>
+          <SkillItem
+            imagePath="/../public/assets/skills/html.png"
+            skillName="HTML"
+          />
 
-              <div className="flex flex-col items-center justify-center">
-                <h3>HTML</h3>
-              </div>
-            </div>
-          </div>
+          <SkillItem
+            imagePath="/../public/assets/skills/css.png"
+            skillName="CSS"
+          />
 
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="/../public/assets/skills/css.png"
-                  width="64px"
-                  height="64px"
-                  alt="/"
-                />
-              </div>
+          <SkillItem
+            imagePath="/../public/assets/skills/tailwind.png"
+            skillName="Tailwind"
+          />
 
-              <div className="flex flex-col items-center justify-center">
-                <h3>CSS</h3>
-              </div>
-            </div>
-          </div>
+          <SkillItem
+            imagePath="/../public/assets/skills/javascript.png"
+            skillName="Javascript"
+          />
 
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="/../public/assets/skills/tailwind.png"
-                  width="64px"
-                  height="64px"
-                  alt="/"
-                />
-              </div>
+          <SkillItem
+            imagePath="/../public/assets/skills/firebase.png"
+            skillName="Firebase"
+          />
 
-              <div className="flex flex-col items-center justify-center">
-                <h3>Tailwind</h3>
-              </div>
-            </div>
-          </div>
+          <SkillItem
+            imagePath="/../public/assets/skills/react.png"
+            skillName="ReactJS"
+          />
 
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="/../public/assets/skills/javascript.png"
-                  width="64px"
-                  height="64px"
-                  alt="/"
-                />
-              </div>
+          <SkillItem
+            imagePath="/../public/assets/skills/node.png"
+            skillName="Node.js"
+          />
 
-              <div className="flex flex-col items-center justify-center">
-                <h3>Javascript</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="/../public/assets/skills/react.png"
-                  width="64px"
-                  height="64px"
-                  alt="/"
-                />
-              </div>
-
-              <div className="flex flex-col items-center justify-center">
-                <h3>React</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="/../public/assets/skills/firebase.png"
-                  width="64px"
-                  height="64px"
-                  alt="/"
-                />
-              </div>
-
-              <div className="flex flex-col items-center justify-center">
-                <h3>Firebase</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="/../public/assets/skills/node.png"
-                  width="64px"
-                  height="64px"
-                  alt="/"
-                />
-              </div>
-
-              <div className="flex flex-col items-center justify-center">
-                <h3>Node.js</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="/../public/assets/skills/github1.png"
-                  width="64px"
-                  height="64px"
-                  alt="/"
-                />
-              </div>
-
-              <div className="flex flex-col items-center justify-center">
-                <h3>Github</h3>
-              </div>
-            </div>
-          </div>
+          <SkillItem
+            imagePath="/../public/assets/skills/github1.png"
+            skillName="Github"
+          />
         </div>
       </div>
     </div>
